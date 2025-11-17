98ead89d35bef27b085ce2e6d623452852864c9c
Marco Battistini <marco.battistini9@studio.unibo.it>
2025-11-17 12:26:57 +0100

    Added fields, getter and setter in Controller

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,21 @@
 package it.unibo.mvc;
 
+import java.io.File;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    private static final String HOME = System.getProperty("user.home");
+    private static final String SEPARATOR = System.getProperty("file.separator");
 
+    private File currentFile = new File(HOME + SEPARATOR + "output.txt");
+
+    public void setCurrentFile(final File newFile) {
+        this.currentFile = newFile;
+    }
+
+    public File getCurrentFile() {
+        return this.currentFile;
+    }
 }
