44d5b0f8c432991cd4d5198b545bca24d3d51732
Marco Battistini <marco.battistini9@studio.unibo.it>
2025-11-23 21:51:54 +0100

    Implemented the Controller interface

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,36 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
- *
+ * Interface of IO controller
  */
 public interface Controller {
 
+    /**
+     * Sets the next string to be printed. Null values are non accepted.
+     * 
+     * @param nextString the string to be set as the next string
+     * @throws NullPointerException if the argument is null
+     */
+    void setNextString(String nextString);
+
+    /**
+     * Gets the next string to be printed.
+     * 
+     * @return the next string to be printed
+     */
+    String getNextString();
+
+    /**
+     * Gets the history of the printed strings.
+     * 
+     * @return a list containing all the string that have been printed
+     */
+    List<String> getHistory();
+
+    /**
+     * Prints the current string.
+     */
+    void printString();
 }
