79f51fb2da315188df5abaa37225bebfc5e51c56
Marco Battistini <marco.battistini9@studio.unibo.it>
2025-11-23 21:56:38 +0100

    Added the unimplemented methods of SimpleController

--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,34 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
- * 
- *
+ * Implementation of the controller interface.
  */
 public final class SimpleController implements Controller {
+
+    @Override
+    public void setNextString(String nextString) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setNextString'");
+    }
+
+    @Override
+    public String getNextString() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getNextString'");
+    }
+
+    @Override
+    public List<String> getHistory() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getHistory'");
+    }
+
+    @Override
+    public void printString() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'printString'");
+    }
 
 }
