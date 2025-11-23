7d739ab17cc5722c688ec53555675c923ed94013
Marco Battistini <marco.battistini9@studio.unibo.it>
2025-11-23 22:01:04 +0100

    Implemented the setNextString method

--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,16 +1,21 @@
 package it.unibo.mvc;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Implementation of the controller interface.
  */
 public final class SimpleController implements Controller {
 
+    private final List<String> history = new ArrayList<>();
+    private String nextString;
+
     @Override
-    public void setNextString(String nextString) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setNextString'");
+    public void setNextString(final String nextString) {
+        Objects.requireNonNull(nextString);
+        this.nextString = nextString;
     }
 
     @Override
