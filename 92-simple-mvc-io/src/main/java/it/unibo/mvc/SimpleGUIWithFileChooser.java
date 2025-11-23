cd1cdcbb6fec526aa3320870137dad7da2a00bf2
Marco Battistini <marco.battistini9@studio.unibo.it>
2025-11-23 17:42:22 +0100

    Added components and handlers to the GUI in SimpleGUIWithFIleChooser

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
@@ -1,7 +1,17 @@
 package it.unibo.mvc;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 /**
  * A very simple program using a graphical interface.
@@ -10,5 +20,48 @@ import javax.swing.JFrame;
 public final class SimpleGUIWithFileChooser {
 
     private final JFrame frame = new JFrame();
+
+    /**
+     * Creates a new SimpleGUIWithFileChooser.
+     * 
+     * @param controller the controller of the GUI
+     */
+    private SimpleGUIWithFileChooser(final Controller controller) {
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+        final JPanel browsePanel = new JPanel();
+        browsePanel.setLayout(new BorderLayout());
+        final JTextField fileName = new JTextField(controller.getCurrentFilePath());
+        browsePanel.add(fileName, BorderLayout.CENTER);
+        final JButton browseButton = new JButton("Browse");
+        browsePanel.add(browseButton, BorderLayout.LINE_END);
+        canvas.add(browsePanel, BorderLayout.NORTH);
+        final JTextArea textArea = new JTextArea();
+        canvas.add(textArea, BorderLayout.CENTER);
+        final JButton save = new JButton("Save");
+        canvas.add(save, BorderLayout.SOUTH);
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent ignored) {
+                try {
+                    controller.writeString(textArea.getText());
+                } catch (final IOException e) {
+                    JOptionPane.showMessageDialog(frame, e, "IO Error", JOptionPane.ERROR);
+                }
+            }
+        });
+        browseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent ignored) {
+                final JFileChooser chooser = new JFileChooser();
+                final var result = chooser.showSaveDialog(browseButton);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    controller.setCurrentFile(chooser.getSelectedFile());
+                } else if (result != JOptionPane.CANCEL_OPTION) {
+                    JOptionPane.showMessageDialog(browseButton, "Error");
+                }
+            }
+        });
+    } 
 
 }
