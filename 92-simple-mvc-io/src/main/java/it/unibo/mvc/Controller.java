1a802f63347f3a6280527222bc62c6fbc792aa17
Marco Battistini <marco.battistini9@studio.unibo.it>
2025-11-23 16:22:54 +0100

    Added the javadoc to the Controller class

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 
-
 /**
  * Application controller. Performs the I/O.
  */
@@ -15,18 +14,39 @@ public class Controller {
 
     private File currentFile = new File(HOME + SEPARATOR + "output.txt");
 
+    /**
+     * Sets a new current file.
+     * 
+     * @param newFile the file to be set as current file
+     */
     public void setCurrentFile(final File newFile) {
         this.currentFile = newFile;
     }
 
+    /**
+     * Gets the current destination file.
+     * 
+     * @return the current file.
+     */
     public File getCurrentFile() {
         return this.currentFile;
     }
 
+    /**
+     * Gets the path of the current destination file.
+     * 
+     * @return the path of the current file
+     */
     public String getCurrentFilePath() {
         return this.currentFile.getAbsolutePath();
     }
 
+    /**
+     * Writes a text into the current file.
+     * 
+     * @param content the string to be written
+     * @throws IOException if the write operation fails
+     */
     public void writeString(final String content) throws IOException {
         try (PrintStream ps = new PrintStream(currentFile, StandardCharsets.UTF_8)) {
             ps.print(content);
