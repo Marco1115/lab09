ae73543f834b4217b02f9970db298c9a84d19045
Marco Battistini <marco.battistini9@studio.unibo.it>
2025-11-24 00:47:31 +0100

    Corrected a spotbugs error in simplecontroller

--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -25,7 +25,7 @@ public final class SimpleController implements Controller {
 
     @Override
     public List<String> getHistory() {
-        return this.history;
+        return List.copyOf(this.history);
     }
 
     @Override
